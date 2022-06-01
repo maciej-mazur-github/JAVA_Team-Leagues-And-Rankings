1003e85915551bbc1f8b875ae698276513700db7
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-06-01 15:29:11 +0200

    Add Player class

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String playerName;
+
+    public Player(String name) {
+        this.playerName = name;
+    }
+
+    public String getPlayerName() {
+        return this.playerName;
+    }
+}
