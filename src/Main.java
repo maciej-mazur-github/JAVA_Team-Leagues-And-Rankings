4a7960d063f6426ad00ab8515e6f5011d034f8aa
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-06-02 16:13:49 +0200

    Implement Comparable to Team. Testing

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        BaseballPlayer joe = new BaseballPlayer("Joe");
+        FootballPlayer beckham = new FootballPlayer("Beckham");
+        BasketballPlayer jordan = new BasketballPlayer("Jordan");
+        BaseballPlayer joe2 = new BaseballPlayer("Joe");
+
+        Team<BaseballPlayer> crows = new Team<>("Crows");
+        crows.addPlayer(joe);
+
+        Team<FootballPlayer> manchester = new Team<>("Manchester");
+        manchester.addPlayer(beckham);
+
+        Team<FootballPlayer> barcelona = new Team<>("Barcelona");
+        FootballPlayer ronaldo = new FootballPlayer("Ronaldo");
+        manchester.addPlayer(ronaldo);
+
+        Team<FootballPlayer> united = new Team<>("United");
+        FootballPlayer lewandowski = new FootballPlayer("Lewandowski");
+        manchester.addPlayer(lewandowski);
+
+        manchester.matchResult(barcelona, 4, 2);
+        manchester.matchResult(united, 2, 5);
+
+        barcelona.matchResult(united, 1, 4);
+        barcelona.matchResult(manchester, 5, 4);
+        //barcelona.matchResult(crows, 3, 6);
+
+        /*Team<String> brokenTeam = new Team<>("Broken Team");
+        brokenTeam.addPlayer("no-one");*/
+
+        //crows.addPlayer(jordan);
+
+        //System.out.println(crows.getTeamCount());
+
+        System.out.println("\n\nRankings:");
+        System.out.println(manchester.getName() + " " + manchester.ranking());
+        System.out.println(united.getName() + " " + united.ranking());
+        System.out.println(barcelona.getName() + " " + barcelona.ranking());
+
+    }
+}
--- src/Team.java
+++ src/Team.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String name;
     private int played = 0;
     private int won = 0;
     private int lost = 0;
     private int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -17,7 +17,7 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if(members.contains(player)) {
             System.out.println(player.getPlayerName() + " is already on this team");
             return false;
@@ -32,21 +32,42 @@ public class Team {
         return members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+        String message;
+
         if(ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if(ourScore < theirScore) {
             lost++;
+            message = " lost to ";
         } else {
             tied++;
+            message = " drew with ";
         }
 
         played++;
-        opponent.matchResult(null, theirScore, ourScore);
+
+        if(opponent != null) {
+            System.out.println(getName() + message + opponent.getName());
+            opponent.matchResult(null, theirScore, ourScore);
+        }
+
     }
 
     public int ranking() {
         return 2 * won + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> opponent) {
+        if(this.ranking() > opponent.ranking()) {
+            return 1;
+        } else if(this.ranking() < opponent.ranking()) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
 
